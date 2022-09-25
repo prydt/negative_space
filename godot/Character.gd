89bf3454c040bd2b4c8bf635bddc03a86a5b8b79
prydt <pranoydll@gmail.com>
2022-09-25 18:22:44 -0500

    final game submission

--- godot/Character.gd
+++ godot/Character.gd
@@ -4,6 +4,14 @@ export var speed = 400
 var screen_size
 var camera
 var animationPlayer
+var animatedSprite
+
+var audioStreamPlayer
+
+var weapon
+var lantern
+
+var health = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +21,12 @@ func _ready():
 	camera = $Camera2D
 	animationPlayer = $AnimationPlayer
 	animationPlayer.play("idle")
+	animatedSprite = $AnimationPlayer/AnimatedSprite
 	
+	audioStreamPlayer = $AudioStreamPlayer2D
+	
+	weapon = $AnimationPlayer/Weapon
+	lantern = $AnimationPlayer/Light2D
 
 func _input(event):
 	pass
@@ -22,21 +35,35 @@ func _input(event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO
+	var isMoving = false
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		isMoving = true
+		animatedSprite.animation = "up walking"
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		isMoving = true
+		animatedSprite.animation = "walking frames"
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		isMoving = true
+		animatedSprite.animation = "left walking"
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		isMoving = true
+		animatedSprite.animation = "right walking"
+	if isMoving == false:
+		animatedSprite.animation = "idle frames"
 		
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 	
+	animatedSprite.position += velocity * delta
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-	
-	#camera.position = position
+	weapon.position = position
+	lantern.position = position
+
+	# play music
+	if !audioStreamPlayer.is_playing():
+		audioStreamPlayer.play()
--- godot/Weapon.gd
+++ godot/Weapon.gd
@@ -1,22 +1,19 @@
-extends Area2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+extends KinematicBody2D
 
 var character
-var camera
 var screen_size
+
+var low_pass_filter
+var audio_stream_player
+
+var mouse_moving
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	character = get_parent()
-	camera = get_parent().get_node("Camera2D")
 	screen_size = get_viewport_rect().size
-	#position = Vector2.ZERO
 	
 	get_tree().get_root().connect("size_changed", self, "handle_resize")
+	mouse_moving = false
 
 
 func handle_resize():
@@ -25,20 +22,20 @@ func handle_resize():
 
 
 func _input(event):
+	
+	#ow_pass_filter.cutoff_hz = clamp(lerp(low_pass_filter.cutoff_hz, 0, 1), 0, 20000)
+	
 	if event is InputEventMouse:
-		#rotation_degrees = character.position.angle_to(event.position)
-		#rotation_degrees = rad2deg(character.position.angle_to_point(event.position))
 		rotation_degrees = rad2deg(atan2(event.position.y - screen_size.y / 2 , 
 										 event.position.x - screen_size. x / 2)) - 90
-		#print("character position: ", character.position)
-		#print("mouse position: ", event.global_position)
-		#rotation_degrees = 30
-		#scale = (character.position - event.position).normalized() * 5
-		#scale = scale.normalized() * character.position.distance_to(event.position) / 100
+										
 		scale.y = (screen_size / 2).distance_to(event.position) / 200
-		
 
+
+		
+		
+	if event is InputEventMouseMotion:
+		var m_speed = event.speed
 
 func _process(delta):
-	#position = character.position
 	pass
